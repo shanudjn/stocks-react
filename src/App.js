cb83c75a88b4aca4a56593f27bb1412135bf4493
Shahazad <shanudjn@gmail.com>
2021-02-02 19:14:17 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var [day, setDay] = useState(0);
+  var [month, setMonth] = useState(0);
+  var [year, setYear] = useState(0);
+
   return (
     <div className="App">
       <h1>
@@ -9,9 +13,21 @@ export default function App() {
       </h1>
       <div className="div-dob">
         <p>Enter your Date of Birth</p>
-        <input className="day"></input>
-        <input className="month"></input>
-        <input className="year"></input>
+        <input
+          className="day"
+          placeholder="Day"
+          type="number"
+          min="1"
+          max="31"
+        ></input>
+        <input
+          className="month"
+          placeholder="Month"
+          type="number"
+          min="1"
+          max="12"
+        ></input>
+        <input className="year" placeholder="Year" type="number"></input>
       </div>
       <div className="div-lucky-number">
         <p>Enter your lucky number</p>
