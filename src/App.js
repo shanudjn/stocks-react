313f874e1463a065d46464b1f1128ad6b5d5d9c3
Shahazad <shanudjn@gmail.com>
2021-02-04 11:08:05 +0530

    feat : added code to get values from input

--- src/App.js
+++ src/App.js
@@ -5,35 +5,87 @@ export default function App() {
   var [day, setDay] = useState(0);
   var [month, setMonth] = useState(0);
   var [year, setYear] = useState(0);
+  var [luckyNumber, setLuckyNumber] = useState(0);
+
+  // function clickHandler(event) {
+  //   console.log(event.target.value);
+  // }
+
+  const getSum = (number) => {
+    var sum = 0;
+    while (number !== 0) {
+      let temp = number % 10;
+      number = Math.floor(number / 10);
+      sum += temp;
+    }
+    return sum;
+  };
+  const handleSubmit = (event) => {
+    console.log(
+      `Day : ${day}
+      Month : ${month}
+      Year : ${year}
+      Lucky Number : ${luckyNumber}`
+    );
+    event.preventDefault();
+    let daySum = getSum(day);
+    let monthSum = getSum(month);
+    let yearSum = getSum(year);
+
+    let total = daySum + monthSum + yearSum;
+
+    console.log(total);
+  };
 
   return (
-    <div className="App">
-      <h1>
-        <span>🗓️</span> Is your birthday lucky? <span>🗓️</span>
-      </h1>
-      <div className="div-dob">
-        <p>Enter your Date of Birth</p>
-        <input
-          className="day"
-          placeholder="Day"
-          type="number"
-          min="1"
-          max="31"
-        ></input>
-        <input
-          className="month"
-          placeholder="Month"
-          type="number"
-          min="1"
-          max="12"
-        ></input>
-        <input className="year" placeholder="Year" type="number"></input>
+    <>
+      <div className="App">
+        <h1>
+          <span>🗓️</span> Is your birthday lucky? <span>🗓️</span>
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="div-dob">
+            <p>Enter your Date of Birth</p>
+            <input
+              className="day"
+              placeholder="Day"
+              type="number"
+              min="1"
+              max="31"
+              value={day}
+              onChange={(e) => setDay(e.target.value)}
+            ></input>
+            <input
+              className="month"
+              placeholder="Month"
+              type="number"
+              min="1"
+              max="12"
+              value={month}
+              onChange={(e) => setMonth(e.target.value)}
+            ></input>
+
+            <input
+              className="year"
+              placeholder="Year"
+              type="number"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            ></input>
+          </div>
+          <div className="div-lucky-number">
+            <p>Enter your lucky number</p>
+            <input
+              className="lnumber"
+              type="number"
+              value={luckyNumber}
+              onChange={(e) => setLuckyNumber(e.target.value)}
+            ></input>
+          </div>
+          <button>Check</button>
+        </form>
       </div>
-      <div className="div-lucky-number">
-        <p>Enter your lucky number</p>
-        <input className="lnumber"></input>
-      </div>
-      <button>Check</button>
-    </div>
+      <div></div>
+    </>
   );
 }
